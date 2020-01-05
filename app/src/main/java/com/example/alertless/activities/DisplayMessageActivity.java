c251a4326aca64a42a80c0847184d30ec7b0c8ec
Ankit Deora <ankitdeora@fico.com>
2020-01-05 20:33:24 +0530

    Removed date picker from display activity

--- app/src/main/java/com/example/alertless/activities/DisplayMessageActivity.java
+++ app/src/main/java/com/example/alertless/activities/DisplayMessageActivity.java
@@ -34,38 +34,6 @@ public class DisplayMessageActivity extends AppCompatActivity {
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView);
         textView.setText(message);
-
-        CalendarView calendarView = (CalendarView) findViewById(R.id.calendarView);
-
-        calendarView.setOnDayClickListener(new OnDayClickListener() {
-            @Override
-            public void onDayClick(EventDay eventDay) {
-                Calendar c = eventDay.getCalendar();
-                int year = c.get(Calendar.YEAR);
-                int month = c.get(Calendar.MONTH);
-                int day = c.get(Calendar.DAY_OF_MONTH);
-
-                String msg = String.format("|%s-%s-%s|", year, month, day);
-                ToastUtils.showToast(getApplicationContext(), msg);
-            }
-        });
     }
 
-    public void saveSchedule(View v) {
-
-        CalendarView calendarView = (CalendarView) findViewById(R.id.calendarView);
-
-        List<Calendar> calendars = calendarView.getSelectedDates();
-
-        String msg = "";
-        for (Calendar c : calendars) {
-            int year = c.get(Calendar.YEAR);
-            int month = c.get(Calendar.MONTH);
-            int day = c.get(Calendar.DAY_OF_MONTH);
-
-            msg += String.format("|%s-%s-%s|", year, month, day);
-        }
-
-        ToastUtils.showToast(getApplicationContext(), msg);
-    }
 }
